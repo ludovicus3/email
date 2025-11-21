63220aebedf924261c7fcb3ed66099e160b3846c
Jason Galens <jason@galensfamily.org>
2025-11-21 21:58:12 +0000

    overrides.d not a good config mechanism

--- containers/postfix/entrypoint.sh
+++ containers/postfix/entrypoint.sh
@@ -2,11 +2,6 @@
 
 shopt -s nullglob
 
-for override in /etc/postfix/overrides.d/*.cf; do
-  echo "applying overrides from $override"
-  /usr/sbin/postconf -e $(cat $override)
-done
-
 for map in /etc/postfix/maps.d/*; do
   echo "hashing map $map"
   /usr/sbin/postmap $map
